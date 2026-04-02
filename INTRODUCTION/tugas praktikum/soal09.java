4d9f7f6ea1ee2ca80a2ac263aaa0a3a6d4ddb775
aldi <aldisalam0756@gmail.com>
2026-04-02 09:44:14 +0800

    soal 09
    9. Buatlah sebuah class kemudian di dalam class tersebut buatlah masing-masing 100 variable untuk empat jenis variable (instance variables, class variables, local variables, dan parameters) jadi total ada 400 variables yang dibuat dengan berbagai varian modifier visibilitas.

---
+++ INTRODUCTION/tugas praktikum/soal09.java
@@ -0,0 +1,85 @@
+package praktikum1;
+public class soal9 {
+
+    // instance variables
+    public int iv1;   private int iv2;   protected int iv3;   int iv4;
+    public int iv5;   private int iv6;   protected int iv7;   int iv8;
+    public int iv9;   private int iv10;  protected int iv11;  int iv12;
+    public int iv13;  private int iv14;  protected int iv15;  int iv16;
+    public int iv17;  private int iv18;  protected int iv19;  int iv20;
+    public int iv21;  private int iv22;  protected int iv23;  int iv24;
+    public int iv25;  private int iv26;  protected int iv27;  int iv28;
+    public int iv29;  private int iv30;  protected int iv31;  int iv32;
+    public int iv33;  private int iv34;  protected int iv35;  int iv36;
+    public int iv37;  private int iv38;  protected int iv39;  int iv40;
+    public int iv41;  private int iv42;  protected int iv43;  int iv44;
+    public int iv45;  private int iv46;  protected int iv47;  int iv48;
+    public int iv49;  private int iv50;  protected int iv51;  int iv52;
+    public int iv53;  private int iv54;  protected int iv55;  int iv56;
+    public int iv57;  private int iv58;  protected int iv59;  int iv60;
+    public int iv61;  private int iv62;  protected int iv63;  int iv64;
+    public int iv65;  private int iv66;  protected int iv67;  int iv68;
+    public int iv69;  private int iv70;  protected int iv71;  int iv72;
+    public int iv73;  private int iv74;  protected int iv75;  int iv76;
+    public int iv77;  private int iv78;  protected int iv79;  int iv80;
+    public int iv81;  private int iv82;  protected int iv83;  int iv84;
+    public int iv85;  private int iv86;  protected int iv87;  int iv88;
+    public int iv89;  private int iv90;  protected int iv91;  int iv92;
+    public int iv93;  private int iv94;  protected int iv95;  int iv96;
+    public int iv97;  private int iv98;  protected int iv99;  int iv100;
+
+    // class variables
+    public static int cv1;   private static int cv2;   protected static int cv3;   static int cv4;
+    public static int cv5;   private static int cv6;   protected static int cv7;   static int cv8;
+    public static int cv9;   private static int cv10;  protected static int cv11;  static int cv12;
+    public static int cv13;  private static int cv14;  protected static int cv15;  static int cv16;
+    public static int cv17;  private static int cv18;  protected static int cv19;  static int cv20;
+    public static int cv21;  private static int cv22;  protected static int cv23;  static int cv24;
+    public static int cv25;  private static int cv26;  protected static int cv27;  static int cv28;
+    public static int cv29;  private static int cv30;  protected static int cv31;  static int cv32;
+    public static int cv33;  private static int cv34;  protected static int cv35;  static int cv36;
+    public static int cv37;  private static int cv38;  protected static int cv39;  static int cv40;
+    public static int cv41;  private static int cv42;  protected static int cv43;  static int cv44;
+    public static int cv45;  private static int cv46;  protected static int cv47;  static int cv48;
+    public static int cv49;  private static int cv50;  protected static int cv51;  static int cv52;
+    public static int cv53;  private static int cv54;  protected static int cv55;  static int cv56;
+    public static int cv57;  private static int cv58;  protected static int cv59;  static int cv60;
+    public static int cv61;  private static int cv62;  protected static int cv63;  static int cv64;
+    public static int cv65;  private static int cv66;  protected static int cv67;  static int cv68;
+    public static int cv69;  private static int cv70;  protected static int cv71;  static int cv72;
+    public static int cv73;  private static int cv74;  protected static int cv75;  static int cv76;
+    public static int cv77;  private static int cv78;  protected static int cv79;  static int cv80;
+    public static int cv81;  private static int cv82;  protected static int cv83;  static int cv84;
+    public static int cv85;  private static int cv86;  protected static int cv87;  static int cv88;
+    public static int cv89;  private static int cv90;  protected static int cv91;  static int cv92;
+    public static int cv93;  private static int cv94;  protected static int cv95;  static int cv96;
+    public static int cv97;  private static int cv98;  protected static int cv99;  static int cv100;
+
+    // parameters
+    public void parameterMethod(
+            int p1,int p2,int p3,int p4,int p5,int p6,int p7,int p8,int p9,int p10,
+            int p11,int p12,int p13,int p14,int p15,int p16,int p17,int p18,int p19,int p20,
+            int p21,int p22,int p23,int p24,int p25,int p26,int p27,int p28,int p29,int p30,
+            int p31,int p32,int p33,int p34,int p35,int p36,int p37,int p38,int p39,int p40,
+            int p41,int p42,int p43,int p44,int p45,int p46,int p47,int p48,int p49,int p50,
+            int p51,int p52,int p53,int p54,int p55,int p56,int p57,int p58,int p59,int p60,
+            int p61,int p62,int p63,int p64,int p65,int p66,int p67,int p68,int p69,int p70,
+            int p71,int p72,int p73,int p74,int p75,int p76,int p77,int p78,int p79,int p80,
+            int p81,int p82,int p83,int p84,int p85,int p86,int p87,int p88,int p89,int p90,
+            int p91,int p92,int p93,int p94,int p95,int p96,int p97,int p98,int p99,int p100
+    ) {}
+
+    // local variables
+    public void localMethod() {
+        int l1,l2,l3,l4,l5,l6,l7,l8,l9,l10;
+        int l11,l12,l13,l14,l15,l16,l17,l18,l19,l20;
+        int l21,l22,l23,l24,l25,l26,l27,l28,l29,l30;
+        int l31,l32,l33,l34,l35,l36,l37,l38,l39,l40;
+        int l41,l42,l43,l44,l45,l46,l47,l48,l49,l50;
+        int l51,l52,l53,l54,l55,l56,l57,l58,l59,l60;
+        int l61,l62,l63,l64,l65,l66,l67,l68,l69,l70;
+        int l71,l72,l73,l74,l75,l76,l77,l78,l79,l80;
+        int l81,l82,l83,l84,l85,l86,l87,l88,l89,l90;
+        int l91,l92,l93,l94,l95,l96,l97,l98,l99,l100;
+    }
+}
