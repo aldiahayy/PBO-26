5fac14f5e81279410000ee4b1b4c8e1e5ff7a1c9
aldi <aldisalam0756@gmail.com>
2026-04-18 01:25:54 +0800

    class Ayam

---
+++ CLASS & OBJECT/tugas praktikum/soal01/Ayam.java
@@ -0,0 +1,9 @@
+package praktikum3.soal01;
+public class Ayam {
+    public String warna;
+    public int umur;
+
+    public void berkokok() {
+        System.out.println("Ayam berkokok");
+    }
+}
