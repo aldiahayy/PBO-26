269c4ffb2a849f7a438788f3625e69b2c3c47d86
aldi <aldisalam0756@gmail.com>
2026-04-22 11:49:31 +0800

    class Queen

---
+++ CLASS & OBJECT/tugas praktikum/soal01/Queen.java
@@ -0,0 +1,9 @@
+package praktikum3.soal01;
+public class Queen {
+    String nama;
+    String kerajaan;
+
+    public void perintah() {
+        System.out.println("Ratu memberi perintah");
+    }
+}
