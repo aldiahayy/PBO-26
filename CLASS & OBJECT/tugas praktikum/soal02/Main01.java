e7b256297c434889fb67fa2679b94c5de2270665
aldi <aldisalam0756@gmail.com>
2026-04-23 09:15:38 +0800

    Main 01

---
+++ CLASS & OBJECT/tugas praktikum/soal02/Main01.java
@@ -0,0 +1,38 @@
+package praktikum3.soal02;
+import praktikum3.soal01.Ayam;
+public class Main01 {
+    public static void main(String[] args) {
+        Ayam object001 = new Ayam(); object001.berkokok();
+        Ayam object002 = new Ayam(); object002.berkokok();
+        Ayam object003 = new Ayam(); object003.berkokok();
+        Ayam object004 = new Ayam(); object004.berkokok();
+        Ayam object005 = new Ayam(); object005.berkokok();
+        Ayam object006 = new Ayam(); object006.berkokok();
+        Ayam object007 = new Ayam(); object007.berkokok();
+        Ayam object008 = new Ayam(); object008.berkokok();
+        Ayam object009 = new Ayam(); object009.berkokok();
+        Ayam object010 = new Ayam(); object010.berkokok();
+
+        Ayam object011 = new Ayam(); object011.berkokok();
+        Ayam object012 = new Ayam(); object012.berkokok();
+        Ayam object013 = new Ayam(); object013.berkokok();
+        Ayam object014 = new Ayam(); object014.berkokok();
+        Ayam object015 = new Ayam(); object015.berkokok();
+        Ayam object016 = new Ayam(); object016.berkokok();
+        Ayam object017 = new Ayam(); object017.berkokok();
+        Ayam object018 = new Ayam(); object018.berkokok();
+        Ayam object019 = new Ayam(); object019.berkokok();
+        Ayam object020 = new Ayam(); object020.berkokok();
+
+        Ayam object021 = new Ayam(); object021.berkokok();
+        Ayam object022 = new Ayam(); object022.berkokok();
+        Ayam object023 = new Ayam(); object023.berkokok();
+        Ayam object024 = new Ayam(); object024.berkokok();
+        Ayam object025 = new Ayam(); object025.berkokok();
+        Ayam object026 = new Ayam(); object026.berkokok();
+        Ayam object027 = new Ayam(); object027.berkokok();
+        Ayam object028 = new Ayam(); object028.berkokok();
+        Ayam object029 = new Ayam(); object029.berkokok();
+        Ayam object030 = new Ayam(); object030.berkokok();
+    }
+}
