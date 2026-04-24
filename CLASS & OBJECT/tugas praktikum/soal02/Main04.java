9c1361b13c640c6dfe563cbb24623bd35c6d8d1f
aldi <aldisalam0756@gmail.com>
2026-04-24 10:02:28 +0800

    class Main04

---
+++ CLASS & OBJECT/tugas praktikum/soal02/Main04.java
@@ -0,0 +1,38 @@
+package praktikum3.soal02;
+import praktikum3.soal01.Dokter;
+public class Main04 {
+    public static void main(String[] args) {
+        Dokter object091 = new Dokter(); object091.periksa();
+        Dokter object092 = new Dokter(); object092.periksa();
+        Dokter object093 = new Dokter(); object093.periksa();
+        Dokter object094 = new Dokter(); object094.periksa();
+        Dokter object095 = new Dokter(); object095.periksa();
+        Dokter object096 = new Dokter(); object096.periksa();
+        Dokter object097 = new Dokter(); object097.periksa();
+        Dokter object098 = new Dokter(); object098.periksa();
+        Dokter object099 = new Dokter(); object099.periksa();
+        Dokter object100 = new Dokter(); object100.periksa();
+
+        Dokter object101 = new Dokter(); object101.periksa();
+        Dokter object102 = new Dokter(); object102.periksa();
+        Dokter object103 = new Dokter(); object103.periksa();
+        Dokter object104 = new Dokter(); object104.periksa();
+        Dokter object105 = new Dokter(); object105.periksa();
+        Dokter object106 = new Dokter(); object106.periksa();
+        Dokter object107 = new Dokter(); object107.periksa();
+        Dokter object108 = new Dokter(); object108.periksa();
+        Dokter object109 = new Dokter(); object109.periksa();
+        Dokter object110 = new Dokter(); object110.periksa();
+
+        Dokter object111 = new Dokter(); object111.periksa();
+        Dokter object112 = new Dokter(); object112.periksa();
+        Dokter object113 = new Dokter(); object113.periksa();
+        Dokter object114 = new Dokter(); object114.periksa();
+        Dokter object115 = new Dokter(); object115.periksa();
+        Dokter object116 = new Dokter(); object116.periksa();
+        Dokter object117 = new Dokter(); object117.periksa();
+        Dokter object118 = new Dokter(); object118.periksa();
+        Dokter object119 = new Dokter(); object119.periksa();
+        Dokter object120 = new Dokter(); object120.periksa();
+    }
+}
