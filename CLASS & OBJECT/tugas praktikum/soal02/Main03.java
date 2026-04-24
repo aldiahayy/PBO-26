89fa503e7643fcbe7a9f8d8b0391a28afc2eaebd
aldi <aldisalam0756@gmail.com>
2026-04-24 10:01:56 +0800

    class Main03

---
+++ CLASS & OBJECT/tugas praktikum/soal02/Main03.java
@@ -0,0 +1,38 @@
+package praktikum3.soal02;
+import praktikum3.soal01.Camera;
+public class Main03 {
+    public static void main(String[] args) {
+        Camera object061 = new Camera(); object061.ambilFoto();
+        Camera object062 = new Camera(); object062.ambilFoto();
+        Camera object063 = new Camera(); object063.ambilFoto();
+        Camera object064 = new Camera(); object064.ambilFoto();
+        Camera object065 = new Camera(); object065.ambilFoto();
+        Camera object066 = new Camera(); object066.ambilFoto();
+        Camera object067 = new Camera(); object067.ambilFoto();
+        Camera object068 = new Camera(); object068.ambilFoto();
+        Camera object069 = new Camera(); object069.ambilFoto();
+        Camera object070 = new Camera(); object070.ambilFoto();
+
+        Camera object071 = new Camera(); object071.ambilFoto();
+        Camera object072 = new Camera(); object072.ambilFoto();
+        Camera object073 = new Camera(); object073.ambilFoto();
+        Camera object074 = new Camera(); object074.ambilFoto();
+        Camera object075 = new Camera(); object075.ambilFoto();
+        Camera object076 = new Camera(); object076.ambilFoto();
+        Camera object077 = new Camera(); object077.ambilFoto();
+        Camera object078 = new Camera(); object078.ambilFoto();
+        Camera object079 = new Camera(); object079.ambilFoto();
+        Camera object080 = new Camera(); object080.ambilFoto();
+
+        Camera object081 = new Camera(); object081.ambilFoto();
+        Camera object082 = new Camera(); object082.ambilFoto();
+        Camera object083 = new Camera(); object083.ambilFoto();
+        Camera object084 = new Camera(); object084.ambilFoto();
+        Camera object085 = new Camera(); object085.ambilFoto();
+        Camera object086 = new Camera(); object086.ambilFoto();
+        Camera object087 = new Camera(); object087.ambilFoto();
+        Camera object088 = new Camera(); object088.ambilFoto();
+        Camera object089 = new Camera(); object089.ambilFoto();
+        Camera object090 = new Camera(); object090.ambilFoto();
+    }
+}
