fc039c5d852b714d143b4ed9804bde5bfbb7761a
aldi <aldisalam0756@gmail.com>
2026-04-24 10:01:08 +0800

    class Main02

---
+++ CLASS & OBJECT/tugas praktikum/soal02/Main02.java
@@ -0,0 +1,38 @@
+package praktikum3.soal02;
+import praktikum3.soal01.Buku;
+public class Main02 {
+    public static void main(String[] args) {
+        Buku object031 = new Buku(); object031.baca();
+        Buku object032 = new Buku(); object032.baca();
+        Buku object033 = new Buku(); object033.baca();
+        Buku object034 = new Buku(); object034.baca();
+        Buku object035 = new Buku(); object035.baca();
+        Buku object036 = new Buku(); object036.baca();
+        Buku object037 = new Buku(); object037.baca();
+        Buku object038 = new Buku(); object038.baca();
+        Buku object039 = new Buku(); object039.baca();
+        Buku object040 = new Buku(); object040.baca();
+
+        Buku object041 = new Buku(); object041.baca();
+        Buku object042 = new Buku(); object042.baca();
+        Buku object043 = new Buku(); object043.baca();
+        Buku object044 = new Buku(); object044.baca();
+        Buku object045 = new Buku(); object045.baca();
+        Buku object046 = new Buku(); object046.baca();
+        Buku object047 = new Buku(); object047.baca();
+        Buku object048 = new Buku(); object048.baca();
+        Buku object049 = new Buku(); object049.baca();
+        Buku object050 = new Buku(); object050.baca();
+
+        Buku object051 = new Buku(); object051.baca();
+        Buku object052 = new Buku(); object052.baca();
+        Buku object053 = new Buku(); object053.baca();
+        Buku object054 = new Buku(); object054.baca();
+        Buku object055 = new Buku(); object055.baca();
+        Buku object056 = new Buku(); object056.baca();
+        Buku object057 = new Buku(); object057.baca();
+        Buku object058 = new Buku(); object058.baca();
+        Buku object059 = new Buku(); object059.baca();
+        Buku object060 = new Buku(); object060.baca();
+    }
+}
