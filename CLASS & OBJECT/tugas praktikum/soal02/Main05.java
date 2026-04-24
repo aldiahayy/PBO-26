a574db679272dd1c0a0e8413fd41a7407d65926c
aldi <aldisalam0756@gmail.com>
2026-04-24 10:02:57 +0800

    class Main05

---
+++ CLASS & OBJECT/tugas praktikum/soal02/Main05.java
@@ -0,0 +1,38 @@
+package praktikum3.soal02;
+import praktikum3.soal01.EsKrim;
+public class Main05 {
+    public static void main(String[] args) {
+        EsKrim object121 = new EsKrim(); object121.makan();
+        EsKrim object122 = new EsKrim(); object122.makan();
+        EsKrim object123 = new EsKrim(); object123.makan();
+        EsKrim object124 = new EsKrim(); object124.makan();
+        EsKrim object125 = new EsKrim(); object125.makan();
+        EsKrim object126 = new EsKrim(); object126.makan();
+        EsKrim object127 = new EsKrim(); object127.makan();
+        EsKrim object128 = new EsKrim(); object128.makan();
+        EsKrim object129 = new EsKrim(); object129.makan();
+        EsKrim object130 = new EsKrim(); object130.makan();
+
+        EsKrim object131 = new EsKrim(); object131.makan();
+        EsKrim object132 = new EsKrim(); object132.makan();
+        EsKrim object133 = new EsKrim(); object133.makan();
+        EsKrim object134 = new EsKrim(); object134.makan();
+        EsKrim object135 = new EsKrim(); object135.makan();
+        EsKrim object136 = new EsKrim(); object136.makan();
+        EsKrim object137 = new EsKrim(); object137.makan();
+        EsKrim object138 = new EsKrim(); object138.makan();
+        EsKrim object139 = new EsKrim(); object139.makan();
+        EsKrim object140 = new EsKrim(); object140.makan();
+
+        EsKrim object141 = new EsKrim(); object141.makan();
+        EsKrim object142 = new EsKrim(); object142.makan();
+        EsKrim object143 = new EsKrim(); object143.makan();
+        EsKrim object144 = new EsKrim(); object144.makan();
+        EsKrim object145 = new EsKrim(); object145.makan();
+        EsKrim object146 = new EsKrim(); object146.makan();
+        EsKrim object147 = new EsKrim(); object147.makan();
+        EsKrim object148 = new EsKrim(); object148.makan();
+        EsKrim object149 = new EsKrim(); object149.makan();
+        EsKrim object150 = new EsKrim(); object150.makan();
+    }
+}
