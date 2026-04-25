d92057f2e1d883e065adea3a3657a1f6e0b2c45c
aldi <aldisalam0756@gmail.com>
2026-04-25 20:00:08 +0800

    class Main08

---
+++ CLASS & OBJECT/tugas praktikum/soal02/Main08.java
@@ -0,0 +1,38 @@
+package praktikum3.soal02;
+import praktikum3.soal01.Hewan;
+public class Main08 {
+    public static void main(String[] args) {
+        Hewan object211 = new Hewan(); object211.bergerak();
+        Hewan object212 = new Hewan(); object212.bergerak();
+        Hewan object213 = new Hewan(); object213.bergerak();
+        Hewan object214 = new Hewan(); object214.bergerak();
+        Hewan object215 = new Hewan(); object215.bergerak();
+        Hewan object216 = new Hewan(); object216.bergerak();
+        Hewan object217 = new Hewan(); object217.bergerak();
+        Hewan object218 = new Hewan(); object218.bergerak();
+        Hewan object219 = new Hewan(); object219.bergerak();
+        Hewan object220 = new Hewan(); object220.bergerak();
+
+        Hewan object221 = new Hewan(); object221.bergerak();
+        Hewan object222 = new Hewan(); object222.bergerak();
+        Hewan object223 = new Hewan(); object223.bergerak();
+        Hewan object224 = new Hewan(); object224.bergerak();
+        Hewan object225 = new Hewan(); object225.bergerak();
+        Hewan object226 = new Hewan(); object226.bergerak();
+        Hewan object227 = new Hewan(); object227.bergerak();
+        Hewan object228 = new Hewan(); object228.bergerak();
+        Hewan object229 = new Hewan(); object229.bergerak();
+        Hewan object230 = new Hewan(); object230.bergerak();
+
+        Hewan object231 = new Hewan(); object231.bergerak();
+        Hewan object232 = new Hewan(); object232.bergerak();
+        Hewan object233 = new Hewan(); object233.bergerak();
+        Hewan object234 = new Hewan(); object234.bergerak();
+        Hewan object235 = new Hewan(); object235.bergerak();
+        Hewan object236 = new Hewan(); object236.bergerak();
+        Hewan object237 = new Hewan(); object237.bergerak();
+        Hewan object238 = new Hewan(); object238.bergerak();
+        Hewan object239 = new Hewan(); object239.bergerak();
+        Hewan object240 = new Hewan(); object240.bergerak();
+    }
+}
