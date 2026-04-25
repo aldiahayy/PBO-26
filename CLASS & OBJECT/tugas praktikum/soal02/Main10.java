a236025034121d283bdea56771944d8fab87b55f
aldi <aldisalam0756@gmail.com>
2026-04-26 00:09:29 +0800

    class Main10

---
+++ CLASS & OBJECT/tugas praktikum/soal02/Main10.java
@@ -0,0 +1,38 @@
+package praktikum3.soal02;
+import praktikum3.soal01.Jam;
+public class Main10 {
+    public static void main(String[] args) {
+        Jam object271 = new Jam(); object271.tampilkan();
+        Jam object272 = new Jam(); object272.tampilkan();
+        Jam object273 = new Jam(); object273.tampilkan();
+        Jam object274 = new Jam(); object274.tampilkan();
+        Jam object275 = new Jam(); object275.tampilkan();
+        Jam object276 = new Jam(); object276.tampilkan();
+        Jam object277 = new Jam(); object277.tampilkan();
+        Jam object278 = new Jam(); object278.tampilkan();
+        Jam object279 = new Jam(); object279.tampilkan();
+        Jam object280 = new Jam(); object280.tampilkan();
+
+        Jam object281 = new Jam(); object281.tampilkan();
+        Jam object282 = new Jam(); object282.tampilkan();
+        Jam object283 = new Jam(); object283.tampilkan();
+        Jam object284 = new Jam(); object284.tampilkan();
+        Jam object285 = new Jam(); object285.tampilkan();
+        Jam object286 = new Jam(); object286.tampilkan();
+        Jam object287 = new Jam(); object287.tampilkan();
+        Jam object288 = new Jam(); object288.tampilkan();
+        Jam object289 = new Jam(); object289.tampilkan();
+        Jam object290 = new Jam(); object290.tampilkan();
+
+        Jam object291 = new Jam(); object291.tampilkan();
+        Jam object292 = new Jam(); object292.tampilkan();
+        Jam object293 = new Jam(); object293.tampilkan();
+        Jam object294 = new Jam(); object294.tampilkan();
+        Jam object295 = new Jam(); object295.tampilkan();
+        Jam object296 = new Jam(); object296.tampilkan();
+        Jam object297 = new Jam(); object297.tampilkan();
+        Jam object298 = new Jam(); object298.tampilkan();
+        Jam object299 = new Jam(); object299.tampilkan();
+        Jam object300 = new Jam(); object300.tampilkan();
+    }
+}
