d355a4692262ba2b88029dc1b7857cd687e7d437
aldi <aldisalam0756@gmail.com>
2026-04-25 19:59:37 +0800

    class Main07

---
+++ CLASS & OBJECT/tugas praktikum/soal02/Main07.java
@@ -0,0 +1,38 @@
+package praktikum3.soal02;
+import praktikum3.soal01.Guru;
+public class Main07 {
+    public static void main(String[] args) {
+        Guru object181 = new Guru(); object181.mengajar();
+        Guru object182 = new Guru(); object182.mengajar();
+        Guru object183 = new Guru(); object183.mengajar();
+        Guru object184 = new Guru(); object184.mengajar();
+        Guru object185 = new Guru(); object185.mengajar();
+        Guru object186 = new Guru(); object186.mengajar();
+        Guru object187 = new Guru(); object187.mengajar();
+        Guru object188 = new Guru(); object188.mengajar();
+        Guru object189 = new Guru(); object189.mengajar();
+        Guru object190 = new Guru(); object190.mengajar();
+
+        Guru object191 = new Guru(); object191.mengajar();
+        Guru object192 = new Guru(); object192.mengajar();
+        Guru object193 = new Guru(); object193.mengajar();
+        Guru object194 = new Guru(); object194.mengajar();
+        Guru object195 = new Guru(); object195.mengajar();
+        Guru object196 = new Guru(); object196.mengajar();
+        Guru object197 = new Guru(); object197.mengajar();
+        Guru object198 = new Guru(); object198.mengajar();
+        Guru object199 = new Guru(); object199.mengajar();
+        Guru object200 = new Guru(); object200.mengajar();
+
+        Guru object201 = new Guru(); object201.mengajar();
+        Guru object202 = new Guru(); object202.mengajar();
+        Guru object203 = new Guru(); object203.mengajar();
+        Guru object204 = new Guru(); object204.mengajar();
+        Guru object205 = new Guru(); object205.mengajar();
+        Guru object206 = new Guru(); object206.mengajar();
+        Guru object207 = new Guru(); object207.mengajar();
+        Guru object208 = new Guru(); object208.mengajar();
+        Guru object209 = new Guru(); object209.mengajar();
+        Guru object210 = new Guru(); object210.mengajar();
+    }
+}
