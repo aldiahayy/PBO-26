3e667ec51f629a1f15129b365edb444cb99ba3ac
aldi <aldisalam0756@gmail.com>
2026-04-26 00:09:55 +0800

    class Main11

---
+++ CLASS & OBJECT/tugas praktikum/soal02/Main11.java
@@ -0,0 +1,38 @@
+package praktikum3.soal02;
+import praktikum3.soal01.Kucing;
+public class Main11 {
+    public static void main(String[] args) {
+        Kucing object301 = new Kucing(); object301.meong();
+        Kucing object302 = new Kucing(); object302.meong();
+        Kucing object303 = new Kucing(); object303.meong();
+        Kucing object304 = new Kucing(); object304.meong();
+        Kucing object305 = new Kucing(); object305.meong();
+        Kucing object306 = new Kucing(); object306.meong();
+        Kucing object307 = new Kucing(); object307.meong();
+        Kucing object308 = new Kucing(); object308.meong();
+        Kucing object309 = new Kucing(); object309.meong();
+        Kucing object310 = new Kucing(); object310.meong();
+
+        Kucing object311 = new Kucing(); object311.meong();
+        Kucing object312 = new Kucing(); object312.meong();
+        Kucing object313 = new Kucing(); object313.meong();
+        Kucing object314 = new Kucing(); object314.meong();
+        Kucing object315 = new Kucing(); object315.meong();
+        Kucing object316 = new Kucing(); object316.meong();
+        Kucing object317 = new Kucing(); object317.meong();
+        Kucing object318 = new Kucing(); object318.meong();
+        Kucing object319 = new Kucing(); object319.meong();
+        Kucing object320 = new Kucing(); object320.meong();
+
+        Kucing object321 = new Kucing(); object321.meong();
+        Kucing object322 = new Kucing(); object322.meong();
+        Kucing object323 = new Kucing(); object323.meong();
+        Kucing object324 = new Kucing(); object324.meong();
+        Kucing object325 = new Kucing(); object325.meong();
+        Kucing object326 = new Kucing(); object326.meong();
+        Kucing object327 = new Kucing(); object327.meong();
+        Kucing object328 = new Kucing(); object328.meong();
+        Kucing object329 = new Kucing(); object329.meong();
+        Kucing object330 = new Kucing(); object330.meong();
+    }
+}
