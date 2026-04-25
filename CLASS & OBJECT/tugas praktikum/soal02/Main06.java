d49520580ee1b3f26c7c610dba9af64eaddc97be
aldi <aldisalam0756@gmail.com>
2026-04-25 19:59:05 +0800

    class Main06

---
+++ CLASS & OBJECT/tugas praktikum/soal02/Main06.java
@@ -0,0 +1,38 @@
+package praktikum3.soal02;
+import praktikum3.soal01.Film;
+public class Main06 {
+    public static void main(String[] args) {
+        Film object151 = new Film(); object151.putar();
+        Film object152 = new Film(); object152.putar();
+        Film object153 = new Film(); object153.putar();
+        Film object154 = new Film(); object154.putar();
+        Film object155 = new Film(); object155.putar();
+        Film object156 = new Film(); object156.putar();
+        Film object157 = new Film(); object157.putar();
+        Film object158 = new Film(); object158.putar();
+        Film object159 = new Film(); object159.putar();
+        Film object160 = new Film(); object160.putar();
+
+        Film object161 = new Film(); object161.putar();
+        Film object162 = new Film(); object162.putar();
+        Film object163 = new Film(); object163.putar();
+        Film object164 = new Film(); object164.putar();
+        Film object165 = new Film(); object165.putar();
+        Film object166 = new Film(); object166.putar();
+        Film object167 = new Film(); object167.putar();
+        Film object168 = new Film(); object168.putar();
+        Film object169 = new Film(); object169.putar();
+        Film object170 = new Film(); object170.putar();
+
+        Film object171 = new Film(); object171.putar();
+        Film object172 = new Film(); object172.putar();
+        Film object173 = new Film(); object173.putar();
+        Film object174 = new Film(); object174.putar();
+        Film object175 = new Film(); object175.putar();
+        Film object176 = new Film(); object176.putar();
+        Film object177 = new Film(); object177.putar();
+        Film object178 = new Film(); object178.putar();
+        Film object179 = new Film(); object179.putar();
+        Film object180 = new Film(); object180.putar();
+    }
+}
