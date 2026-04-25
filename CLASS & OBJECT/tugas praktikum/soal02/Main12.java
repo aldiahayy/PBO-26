8ecc8e1ae17c024ce35be508f91918d983a7744d
aldi <aldisalam0756@gmail.com>
2026-04-26 00:10:32 +0800

    class Main12

---
+++ CLASS & OBJECT/tugas praktikum/soal02/Main12.java
@@ -0,0 +1,38 @@
+package praktikum3.soal02;
+import praktikum3.soal01.Laptop;
+public class Main12 {
+    public static void main(String[] args) {
+        Laptop object331 = new Laptop(); object331.nyala();
+        Laptop object332 = new Laptop(); object332.nyala();
+        Laptop object333 = new Laptop(); object333.nyala();
+        Laptop object334 = new Laptop(); object334.nyala();
+        Laptop object335 = new Laptop(); object335.nyala();
+        Laptop object336 = new Laptop(); object336.nyala();
+        Laptop object337 = new Laptop(); object337.nyala();
+        Laptop object338 = new Laptop(); object338.nyala();
+        Laptop object339 = new Laptop(); object339.nyala();
+        Laptop object340 = new Laptop(); object340.nyala();
+
+        Laptop object341 = new Laptop(); object341.nyala();
+        Laptop object342 = new Laptop(); object342.nyala();
+        Laptop object343 = new Laptop(); object343.nyala();
+        Laptop object344 = new Laptop(); object344.nyala();
+        Laptop object345 = new Laptop(); object345.nyala();
+        Laptop object346 = new Laptop(); object346.nyala();
+        Laptop object347 = new Laptop(); object347.nyala();
+        Laptop object348 = new Laptop(); object348.nyala();
+        Laptop object349 = new Laptop(); object349.nyala();
+        Laptop object350 = new Laptop(); object350.nyala();
+
+        Laptop object351 = new Laptop(); object351.nyala();
+        Laptop object352 = new Laptop(); object352.nyala();
+        Laptop object353 = new Laptop(); object353.nyala();
+        Laptop object354 = new Laptop(); object354.nyala();
+        Laptop object355 = new Laptop(); object355.nyala();
+        Laptop object356 = new Laptop(); object356.nyala();
+        Laptop object357 = new Laptop(); object357.nyala();
+        Laptop object358 = new Laptop(); object358.nyala();
+        Laptop object359 = new Laptop(); object359.nyala();
+        Laptop object360 = new Laptop(); object360.nyala();
+    }
+}
