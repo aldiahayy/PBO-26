22c506a8df0005df5a14a390deda45a80466130b
aldi <aldisalam0756@gmail.com>
2026-04-26 00:11:01 +0800

    class Main13

---
+++ CLASS & OBJECT/tugas praktikum/soal02/Main13.java
@@ -0,0 +1,38 @@
+package praktikum3.soal02;
+import praktikum3.soal01.Mobil;
+public class Main13 {
+    public static void main(String[] args) {
+        Mobil object361 = new Mobil(); object361.jalan();
+        Mobil object362 = new Mobil(); object362.jalan();
+        Mobil object363 = new Mobil(); object363.jalan();
+        Mobil object364 = new Mobil(); object364.jalan();
+        Mobil object365 = new Mobil(); object365.jalan();
+        Mobil object366 = new Mobil(); object366.jalan();
+        Mobil object367 = new Mobil(); object367.jalan();
+        Mobil object368 = new Mobil(); object368.jalan();
+        Mobil object369 = new Mobil(); object369.jalan();
+        Mobil object370 = new Mobil(); object370.jalan();
+
+        Mobil object371 = new Mobil(); object371.jalan();
+        Mobil object372 = new Mobil(); object372.jalan();
+        Mobil object373 = new Mobil(); object373.jalan();
+        Mobil object374 = new Mobil(); object374.jalan();
+        Mobil object375 = new Mobil(); object375.jalan();
+        Mobil object376 = new Mobil(); object376.jalan();
+        Mobil object377 = new Mobil(); object377.jalan();
+        Mobil object378 = new Mobil(); object378.jalan();
+        Mobil object379 = new Mobil(); object379.jalan();
+        Mobil object380 = new Mobil(); object380.jalan();
+
+        Mobil object381 = new Mobil(); object381.jalan();
+        Mobil object382 = new Mobil(); object382.jalan();
+        Mobil object383 = new Mobil(); object383.jalan();
+        Mobil object384 = new Mobil(); object384.jalan();
+        Mobil object385 = new Mobil(); object385.jalan();
+        Mobil object386 = new Mobil(); object386.jalan();
+        Mobil object387 = new Mobil(); object387.jalan();
+        Mobil object388 = new Mobil(); object388.jalan();
+        Mobil object389 = new Mobil(); object389.jalan();
+        Mobil object390 = new Mobil(); object390.jalan();
+    }
+}
