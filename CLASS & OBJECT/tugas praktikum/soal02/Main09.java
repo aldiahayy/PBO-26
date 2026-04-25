8be1e4a65af6d672b78ae93a8d37bd667f54b279
aldi <aldisalam0756@gmail.com>
2026-04-25 20:01:46 +0800

    class Main09

---
+++ CLASS & OBJECT/tugas praktikum/soal02/Main09.java
@@ -0,0 +1,38 @@
+package praktikum3.soal02;
+import praktikum3.soal01.Ikan;
+public class Main09 {
+    public static void main(String[] args) {
+        Ikan object241 = new Ikan(); object241.berenang();
+        Ikan object242 = new Ikan(); object242.berenang();
+        Ikan object243 = new Ikan(); object243.berenang();
+        Ikan object244 = new Ikan(); object244.berenang();
+        Ikan object245 = new Ikan(); object245.berenang();
+        Ikan object246 = new Ikan(); object246.berenang();
+        Ikan object247 = new Ikan(); object247.berenang();
+        Ikan object248 = new Ikan(); object248.berenang();
+        Ikan object249 = new Ikan(); object249.berenang();
+        Ikan object250 = new Ikan(); object250.berenang();
+
+        Ikan object251 = new Ikan(); object251.berenang();
+        Ikan object252 = new Ikan(); object252.berenang();
+        Ikan object253 = new Ikan(); object253.berenang();
+        Ikan object254 = new Ikan(); object254.berenang();
+        Ikan object255 = new Ikan(); object255.berenang();
+        Ikan object256 = new Ikan(); object256.berenang();
+        Ikan object257 = new Ikan(); object257.berenang();
+        Ikan object258 = new Ikan(); object258.berenang();
+        Ikan object259 = new Ikan(); object259.berenang();
+        Ikan object260 = new Ikan(); object260.berenang();
+
+        Ikan object261 = new Ikan(); object261.berenang();
+        Ikan object262 = new Ikan(); object262.berenang();
+        Ikan object263 = new Ikan(); object263.berenang();
+        Ikan object264 = new Ikan(); object264.berenang();
+        Ikan object265 = new Ikan(); object265.berenang();
+        Ikan object266 = new Ikan(); object266.berenang();
+        Ikan object267 = new Ikan(); object267.berenang();
+        Ikan object268 = new Ikan(); object268.berenang();
+        Ikan object269 = new Ikan(); object269.berenang();
+        Ikan object270 = new Ikan(); object270.berenang();
+    }
+}
