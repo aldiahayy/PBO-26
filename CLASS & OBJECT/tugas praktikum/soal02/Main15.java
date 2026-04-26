b179a4ed4ee1b9f28198f6e6dd45d7fafa8c1cc4
aldi <aldisalam0756@gmail.com>
2026-04-27 03:32:03 +0800

    class Main15

---
+++ CLASS & OBJECT/tugas praktikum/soal02/Main15.java
@@ -0,0 +1,38 @@
+package praktikum3.soal02;
+import praktikum3.soal01.Orang;
+public class Main15 {
+    public static void main(String[] args) {
+        Orang object421 = new Orang(); object421.bicara();
+        Orang object422 = new Orang(); object422.bicara();
+        Orang object423 = new Orang(); object423.bicara();
+        Orang object424 = new Orang(); object424.bicara();
+        Orang object425 = new Orang(); object425.bicara();
+        Orang object426 = new Orang(); object426.bicara();
+        Orang object427 = new Orang(); object427.bicara();
+        Orang object428 = new Orang(); object428.bicara();
+        Orang object429 = new Orang(); object429.bicara();
+        Orang object430 = new Orang(); object430.bicara();
+
+        Orang object431 = new Orang(); object431.bicara();
+        Orang object432 = new Orang(); object432.bicara();
+        Orang object433 = new Orang(); object433.bicara();
+        Orang object434 = new Orang(); object434.bicara();
+        Orang object435 = new Orang(); object435.bicara();
+        Orang object436 = new Orang(); object436.bicara();
+        Orang object437 = new Orang(); object437.bicara();
+        Orang object438 = new Orang(); object438.bicara();
+        Orang object439 = new Orang(); object439.bicara();
+        Orang object440 = new Orang(); object440.bicara();
+
+        Orang object441 = new Orang(); object441.bicara();
+        Orang object442 = new Orang(); object442.bicara();
+        Orang object443 = new Orang(); object443.bicara();
+        Orang object444 = new Orang(); object444.bicara();
+        Orang object445 = new Orang(); object445.bicara();
+        Orang object446 = new Orang(); object446.bicara();
+        Orang object447 = new Orang(); object447.bicara();
+        Orang object448 = new Orang(); object448.bicara();
+        Orang object449 = new Orang(); object449.bicara();
+        Orang object450 = new Orang(); object450.bicara();
+    }
+}
