d74495a09dcea43c7d3a9b813bcdce4658c4372d
aldi <aldisalam0756@gmail.com>
2026-04-27 03:31:29 +0800

    class Main14

---
+++ CLASS & OBJECT/tugas praktikum/soal02/Main14.java
@@ -0,0 +1,38 @@
+package praktikum3.soal02;
+import praktikum3.soal01.Notebook;
+public class Main14 {
+    public static void main(String[] args) {
+        Notebook object391 = new Notebook(); object391.simpanData();
+        Notebook object392 = new Notebook(); object392.simpanData();
+        Notebook object393 = new Notebook(); object393.simpanData();
+        Notebook object394 = new Notebook(); object394.simpanData();
+        Notebook object395 = new Notebook(); object395.simpanData();
+        Notebook object396 = new Notebook(); object396.simpanData();
+        Notebook object397 = new Notebook(); object397.simpanData();
+        Notebook object398 = new Notebook(); object398.simpanData();
+        Notebook object399 = new Notebook(); object399.simpanData();
+        Notebook object400 = new Notebook(); object400.simpanData();
+
+        Notebook object401 = new Notebook(); object401.simpanData();
+        Notebook object402 = new Notebook(); object402.simpanData();
+        Notebook object403 = new Notebook(); object403.simpanData();
+        Notebook object404 = new Notebook(); object404.simpanData();
+        Notebook object405 = new Notebook(); object405.simpanData();
+        Notebook object406 = new Notebook(); object406.simpanData();
+        Notebook object407 = new Notebook(); object407.simpanData();
+        Notebook object408 = new Notebook(); object408.simpanData();
+        Notebook object409 = new Notebook(); object409.simpanData();
+        Notebook object410 = new Notebook(); object410.simpanData();
+
+        Notebook object411 = new Notebook(); object411.simpanData();
+        Notebook object412 = new Notebook(); object412.simpanData();
+        Notebook object413 = new Notebook(); object413.simpanData();
+        Notebook object414 = new Notebook(); object414.simpanData();
+        Notebook object415 = new Notebook(); object415.simpanData();
+        Notebook object416 = new Notebook(); object416.simpanData();
+        Notebook object417 = new Notebook(); object417.simpanData();
+        Notebook object418 = new Notebook(); object418.simpanData();
+        Notebook object419 = new Notebook(); object419.simpanData();
+        Notebook object420 = new Notebook(); object420.simpanData();
+    }
+}
