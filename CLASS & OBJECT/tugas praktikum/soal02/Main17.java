81192579db3686e227596154339f784ee27421c4
aldi <aldisalam0756@gmail.com>
2026-04-27 03:33:09 +0800

    class Main17

---
+++ CLASS & OBJECT/tugas praktikum/soal02/Main17.java
@@ -0,0 +1,38 @@
+package praktikum3.soal02;
+import praktikum3.soal01.Queen;
+public class Main17 {
+    public static void main(String[] args) {
+        Queen object481 = new Queen(); object481.perintah();
+        Queen object482 = new Queen(); object482.perintah();
+        Queen object483 = new Queen(); object483.perintah();
+        Queen object484 = new Queen(); object484.perintah();
+        Queen object485 = new Queen(); object485.perintah();
+        Queen object486 = new Queen(); object486.perintah();
+        Queen object487 = new Queen(); object487.perintah();
+        Queen object488 = new Queen(); object488.perintah();
+        Queen object489 = new Queen(); object489.perintah();
+        Queen object490 = new Queen(); object490.perintah();
+
+        Queen object491 = new Queen(); object491.perintah();
+        Queen object492 = new Queen(); object492.perintah();
+        Queen object493 = new Queen(); object493.perintah();
+        Queen object494 = new Queen(); object494.perintah();
+        Queen object495 = new Queen(); object495.perintah();
+        Queen object496 = new Queen(); object496.perintah();
+        Queen object497 = new Queen(); object497.perintah();
+        Queen object498 = new Queen(); object498.perintah();
+        Queen object499 = new Queen(); object499.perintah();
+        Queen object500 = new Queen(); object500.perintah();
+
+        Queen object501 = new Queen(); object501.perintah();
+        Queen object502 = new Queen(); object502.perintah();
+        Queen object503 = new Queen(); object503.perintah();
+        Queen object504 = new Queen(); object504.perintah();
+        Queen object505 = new Queen(); object505.perintah();
+        Queen object506 = new Queen(); object506.perintah();
+        Queen object507 = new Queen(); object507.perintah();
+        Queen object508 = new Queen(); object508.perintah();
+        Queen object509 = new Queen(); object509.perintah();
+        Queen object510 = new Queen(); object510.perintah();
+    }
+}
