e414b484260b971d6924efdd9733dfb1a8d791ba
aldi <aldisalam0756@gmail.com>
2026-04-27 03:32:33 +0800

    class Main16

---
+++ CLASS & OBJECT/tugas praktikum/soal02/Main16.java
@@ -0,0 +1,38 @@
+package praktikum3.soal02;
+import praktikum3.soal01.Pensil;
+public class Main16 {
+    public static void main(String[] args) {
+        Pensil object451 = new Pensil(); object451.tulis();
+        Pensil object452 = new Pensil(); object452.tulis();
+        Pensil object453 = new Pensil(); object453.tulis();
+        Pensil object454 = new Pensil(); object454.tulis();
+        Pensil object455 = new Pensil(); object455.tulis();
+        Pensil object456 = new Pensil(); object456.tulis();
+        Pensil object457 = new Pensil(); object457.tulis();
+        Pensil object458 = new Pensil(); object458.tulis();
+        Pensil object459 = new Pensil(); object459.tulis();
+        Pensil object460 = new Pensil(); object460.tulis();
+
+        Pensil object461 = new Pensil(); object461.tulis();
+        Pensil object462 = new Pensil(); object462.tulis();
+        Pensil object463 = new Pensil(); object463.tulis();
+        Pensil object464 = new Pensil(); object464.tulis();
+        Pensil object465 = new Pensil(); object465.tulis();
+        Pensil object466 = new Pensil(); object466.tulis();
+        Pensil object467 = new Pensil(); object467.tulis();
+        Pensil object468 = new Pensil(); object468.tulis();
+        Pensil object469 = new Pensil(); object469.tulis();
+        Pensil object470 = new Pensil(); object470.tulis();
+
+        Pensil object471 = new Pensil(); object471.tulis();
+        Pensil object472 = new Pensil(); object472.tulis();
+        Pensil object473 = new Pensil(); object473.tulis();
+        Pensil object474 = new Pensil(); object474.tulis();
+        Pensil object475 = new Pensil(); object475.tulis();
+        Pensil object476 = new Pensil(); object476.tulis();
+        Pensil object477 = new Pensil(); object477.tulis();
+        Pensil object478 = new Pensil(); object478.tulis();
+        Pensil object479 = new Pensil(); object479.tulis();
+        Pensil object480 = new Pensil(); object480.tulis();
+    }
+}
