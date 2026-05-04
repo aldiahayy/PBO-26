aa8dd91a77f33c7762de3a68c7a7b9f406f382ff
aldi <aldisalam0756@gmail.com>
2026-05-05 01:57:05 +0800

    class Main18

---
+++ CLASS & OBJECT/tugas praktikum/soal02/Main18.java
@@ -0,0 +1,38 @@
+package praktikum3.soal02;
+import praktikum3.soal01.Rumah;
+public class Main18 {
+    public static void main(String[] args) {
+        Rumah object511 = new Rumah(); object511.bukaPintu();
+        Rumah object512 = new Rumah(); object512.bukaPintu();
+        Rumah object513 = new Rumah(); object513.bukaPintu();
+        Rumah object514 = new Rumah(); object514.bukaPintu();
+        Rumah object515 = new Rumah(); object515.bukaPintu();
+        Rumah object516 = new Rumah(); object516.bukaPintu();
+        Rumah object517 = new Rumah(); object517.bukaPintu();
+        Rumah object518 = new Rumah(); object518.bukaPintu();
+        Rumah object519 = new Rumah(); object519.bukaPintu();
+        Rumah object520 = new Rumah(); object520.bukaPintu();
+
+        Rumah object521 = new Rumah(); object521.bukaPintu();
+        Rumah object522 = new Rumah(); object522.bukaPintu();
+        Rumah object523 = new Rumah(); object523.bukaPintu();
+        Rumah object524 = new Rumah(); object524.bukaPintu();
+        Rumah object525 = new Rumah(); object525.bukaPintu();
+        Rumah object526 = new Rumah(); object526.bukaPintu();
+        Rumah object527 = new Rumah(); object527.bukaPintu();
+        Rumah object528 = new Rumah(); object528.bukaPintu();
+        Rumah object529 = new Rumah(); object529.bukaPintu();
+        Rumah object530 = new Rumah(); object530.bukaPintu();
+
+        Rumah object531 = new Rumah(); object531.bukaPintu();
+        Rumah object532 = new Rumah(); object532.bukaPintu();
+        Rumah object533 = new Rumah(); object533.bukaPintu();
+        Rumah object534 = new Rumah(); object534.bukaPintu();
+        Rumah object535 = new Rumah(); object535.bukaPintu();
+        Rumah object536 = new Rumah(); object536.bukaPintu();
+        Rumah object537 = new Rumah(); object537.bukaPintu();
+        Rumah object538 = new Rumah(); object538.bukaPintu();
+        Rumah object539 = new Rumah(); object539.bukaPintu();
+        Rumah object540 = new Rumah(); object540.bukaPintu();
+    }
+}
