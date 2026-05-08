0cc99eb01d89930619325f0b74760e7a457e71f7
aldi <aldisalam0756@gmail.com>
2026-05-08 20:01:06 +0800

    class Main20

---
+++ CLASS & OBJECT/tugas praktikum/soal02/Main20.java
@@ -0,0 +1,38 @@
+package praktikum3.soal02;
+import praktikum3.soal01.Televisi;
+public class Main20 {
+    public static void main(String[] args) {
+        Televisi object571 = new Televisi(); object571.nyala();
+        Televisi object572 = new Televisi(); object572.nyala();
+        Televisi object573 = new Televisi(); object573.nyala();
+        Televisi object574 = new Televisi(); object574.nyala();
+        Televisi object575 = new Televisi(); object575.nyala();
+        Televisi object576 = new Televisi(); object576.nyala();
+        Televisi object577 = new Televisi(); object577.nyala();
+        Televisi object578 = new Televisi(); object578.nyala();
+        Televisi object579 = new Televisi(); object579.nyala();
+        Televisi object580 = new Televisi(); object580.nyala();
+
+        Televisi object581 = new Televisi(); object581.nyala();
+        Televisi object582 = new Televisi(); object582.nyala();
+        Televisi object583 = new Televisi(); object583.nyala();
+        Televisi object584 = new Televisi(); object584.nyala();
+        Televisi object585 = new Televisi(); object585.nyala();
+        Televisi object586 = new Televisi(); object586.nyala();
+        Televisi object587 = new Televisi(); object587.nyala();
+        Televisi object588 = new Televisi(); object588.nyala();
+        Televisi object589 = new Televisi(); object589.nyala();
+        Televisi object590 = new Televisi(); object590.nyala();
+
+        Televisi object591 = new Televisi(); object591.nyala();
+        Televisi object592 = new Televisi(); object592.nyala();
+        Televisi object593 = new Televisi(); object593.nyala();
+        Televisi object594 = new Televisi(); object594.nyala();
+        Televisi object595 = new Televisi(); object595.nyala();
+        Televisi object596 = new Televisi(); object596.nyala();
+        Televisi object597 = new Televisi(); object597.nyala();
+        Televisi object598 = new Televisi(); object598.nyala();
+        Televisi object599 = new Televisi(); object599.nyala();
+        Televisi object600 = new Televisi(); object600.nyala();
+    }
+}
