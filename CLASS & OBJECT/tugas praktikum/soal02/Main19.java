5f80b3b042e438b8acc4aeb7ea2d807880d95992
aldi <aldisalam0756@gmail.com>
2026-05-08 20:00:33 +0800

    class Main19

---
+++ CLASS & OBJECT/tugas praktikum/soal02/Main19.java
@@ -0,0 +1,38 @@
+package praktikum3.soal02;
+import praktikum3.soal01.Sepeda;
+public class Main19 {
+    public static void main(String[] args) {
+        Sepeda object541 = new Sepeda(); object541.kayuh();
+        Sepeda object542 = new Sepeda(); object542.kayuh();
+        Sepeda object543 = new Sepeda(); object543.kayuh();
+        Sepeda object544 = new Sepeda(); object544.kayuh();
+        Sepeda object545 = new Sepeda(); object545.kayuh();
+        Sepeda object546 = new Sepeda(); object546.kayuh();
+        Sepeda object547 = new Sepeda(); object547.kayuh();
+        Sepeda object548 = new Sepeda(); object548.kayuh();
+        Sepeda object549 = new Sepeda(); object549.kayuh();
+        Sepeda object550 = new Sepeda(); object550.kayuh();
+
+        Sepeda object551 = new Sepeda(); object551.kayuh();
+        Sepeda object552 = new Sepeda(); object552.kayuh();
+        Sepeda object553 = new Sepeda(); object553.kayuh();
+        Sepeda object554 = new Sepeda(); object554.kayuh();
+        Sepeda object555 = new Sepeda(); object555.kayuh();
+        Sepeda object556 = new Sepeda(); object556.kayuh();
+        Sepeda object557 = new Sepeda(); object557.kayuh();
+        Sepeda object558 = new Sepeda(); object558.kayuh();
+        Sepeda object559 = new Sepeda(); object559.kayuh();
+        Sepeda object560 = new Sepeda(); object560.kayuh();
+
+        Sepeda object561 = new Sepeda(); object561.kayuh();
+        Sepeda object562 = new Sepeda(); object562.kayuh();
+        Sepeda object563 = new Sepeda(); object563.kayuh();
+        Sepeda object564 = new Sepeda(); object564.kayuh();
+        Sepeda object565 = new Sepeda(); object565.kayuh();
+        Sepeda object566 = new Sepeda(); object566.kayuh();
+        Sepeda object567 = new Sepeda(); object567.kayuh();
+        Sepeda object568 = new Sepeda(); object568.kayuh();
+        Sepeda object569 = new Sepeda(); object569.kayuh();
+        Sepeda object570 = new Sepeda(); object570.kayuh();
+    }
+}
