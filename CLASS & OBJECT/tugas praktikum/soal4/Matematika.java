98fee5cb09e53631d4196dfd89167774c12300fa
aldi <aldisalam0756@gmail.com>
2026-05-12 21:32:04 +0800

    class Matematika

---
+++ CLASS & OBJECT/tugas praktikum/soal4/Matematika.java
@@ -0,0 +1,14 @@
+package praktikum3.soal04;
+class Matematika {
+    // static field
+    static int angka = 10;
+
+    // static method
+    static void tampilAngka() {
+        System.out.println("Nilai angka: " + angka);
+    }
+
+    static int tambah(int a, int b) {
+        return a + b;
+    }
+}
