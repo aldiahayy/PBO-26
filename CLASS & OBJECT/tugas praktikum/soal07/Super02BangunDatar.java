43cd111c59357afe6fb4cd9b5c1062636b637d2a
aldi <aldisalam0756@gmail.com>
2026-05-15 13:39:36 +0800

    class Super02BangunDatar

---
+++ CLASS & OBJECT/tugas praktikum/soal07/Super02BangunDatar.java
@@ -0,0 +1,8 @@
+package praktikum3.soal07;
+public class Super02BangunDatar {
+    protected String nama;
+
+    protected void info() {
+        System.out.println("Ini adalah bangun datar");
+    }
+}
