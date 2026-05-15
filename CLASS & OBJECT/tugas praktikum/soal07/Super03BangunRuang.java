4254f9f2a0a88c847f3ee6f2a6826840f4525367
aldi <aldisalam0756@gmail.com>
2026-05-16 02:24:45 +0800

    class Super03BangunRuang

---
+++ CLASS & OBJECT/tugas praktikum/soal07/Super03BangunRuang.java
@@ -0,0 +1,8 @@
+package praktikum3.soal07;
+public class Super03BangunRuang {
+    protected String nama;
+
+    public void info() {
+        System.out.println("Ini adalah bangun ruang");
+    }
+}
