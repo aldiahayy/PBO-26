395f0455f7239407b4b69ca54f5a6376d990b6b9
aldi <aldisalam0756@gmail.com>
2026-05-17 10:30:09 +0800

    class Super07Mahasiswa

---
+++ CLASS & OBJECT/tugas praktikum/soal07/Super07Mahasiswa.java
@@ -0,0 +1,8 @@
+package praktikum3.soal07;
+public class Super07Mahasiswa {
+    protected String nama;
+
+    public void belajar() {
+        System.out.println("Mahasiswa sedang belajar");
+    }
+}
