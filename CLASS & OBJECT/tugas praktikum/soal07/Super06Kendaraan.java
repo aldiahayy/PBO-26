b93e061f02c813033fa8cec86f96bbe8c25bf044
aldi <aldisalam0756@gmail.com>
2026-05-17 10:29:38 +0800

    class Super06Kendaraan

---
+++ CLASS & OBJECT/tugas praktikum/soal07/Super06Kendaraan.java
@@ -0,0 +1,8 @@
+package praktikum3.soal07;
+public class Super06Kendaraan {
+    protected String nama;
+
+    public void jalan() {
+        System.out.println("Kendaraan sedang berjalan");
+    }
+}
