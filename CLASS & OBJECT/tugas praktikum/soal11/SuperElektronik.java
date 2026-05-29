b55acf4652e6f70d5550318d86e0d00f17ac5c78
aldi <aldisalam0756@gmail.com>
2026-05-29 21:09:14 +0800

    class SuperElektronik

---
+++ CLASS & OBJECT/tugas praktikum/soal11/SuperElektronik.java
@@ -0,0 +1,8 @@
+package praktikum3.soal11;
+public class SuperElektronik {
+    protected String nama;
+
+    public void info() {
+        System.out.println("Ini adalah barang elektronik");
+    }
+}
