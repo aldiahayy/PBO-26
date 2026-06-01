e06e850ac6d4d8494cd30aee2f7518bf90884ae8
aldi <aldisalam0756@gmail.com>
2026-06-01 11:01:14 +0800

    class Main

---
+++ ENCAPSULATION/tugas praktikum/soal01,02,03,04,05,06/test/Main.java
@@ -0,0 +1,139 @@
+package praktikum4.test;
+import praktikum4.myprogram.Mahasiswa;
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Mahasiswa> arraylist = new ArrayList<>();
+
+        Mahasiswa object1 = new Mahasiswa();
+        object1.setNama("Aldi");
+        object1.setNim("D0223043");
+        object1.setAlamat("Papalang");
+        arraylist.add(object1);
+
+        Mahasiswa object2 = new Mahasiswa();
+        object2.setNama("");
+        object2.setNim("");
+        object2.setAlamat("");
+        arraylist.add(object2);
+
+        Mahasiswa object3 = new Mahasiswa();
+        object3.setNama("");
+        object3.setNim("");
+        object3.setAlamat("");
+        arraylist.add(object3);
+
+        Mahasiswa object4 = new Mahasiswa();
+        object4.setNama("");
+        object4.setNim("");
+        object4.setAlamat("");
+        arraylist.add(object4);
+
+        Mahasiswa object5 = new Mahasiswa();
+        object5.setNama("");
+        object5.setNim("");
+        object5.setAlamat("");
+        arraylist.add(object5);
+
+        Mahasiswa object6 = new Mahasiswa();
+        object6.setNama("");
+        object6.setNim("");
+        object6.setAlamat("");
+        arraylist.add(object6);
+
+        Mahasiswa object7 = new Mahasiswa();
+        object7.setNama("");
+        object7.setNim("");
+        object7.setAlamat("");
+        arraylist.add(object7);
+
+        Mahasiswa object8 = new Mahasiswa();
+        object8.setNama("");
+        object8.setNim("");
+        object8.setAlamat("");
+        arraylist.add(object8);
+
+        Mahasiswa object9 = new Mahasiswa();
+        object9.setNama("");
+        object9.setNim("");
+        object9.setAlamat("");
+        arraylist.add(object9);
+
+        Mahasiswa object10 = new Mahasiswa();
+        object10.setNama("");
+        object10.setNim("");
+        object10.setAlamat("");
+        arraylist.add(object10);
+
+        Mahasiswa object11 = new Mahasiswa();
+        object11.setNama("");
+        object11.setNim("");
+        object11.setAlamat("");
+        arraylist.add(object11);
+
+        Mahasiswa object12 = new Mahasiswa();
+        object12.setNama("");
+        object12.setNim("");
+        object12.setAlamat("");
+        arraylist.add(object12);
+
+        Mahasiswa object13 = new Mahasiswa();
+        object13.setNama("");
+        object13.setNim("");
+        object13.setAlamat("");
+        arraylist.add(object13);
+
+        Mahasiswa object14 = new Mahasiswa();
+        object14.setNama("");
+        object14.setNim("");
+        object14.setAlamat("");
+        arraylist.add(object14);
+
+        Mahasiswa object15 = new Mahasiswa();
+        object15.setNama("");
+        object15.setNim("");
+        object15.setAlamat("");
+        arraylist.add(object15);
+
+        Mahasiswa object16 = new Mahasiswa();
+        object16.setNama("");
+        object16.setNim("");
+        object16.setAlamat("");
+        arraylist.add(object16);
+
+        Mahasiswa object17 = new Mahasiswa();
+        object17.setNama("");
+        object17.setNim("");
+        object17.setAlamat("");
+        arraylist.add(object17);
+
+        Mahasiswa object18 = new Mahasiswa();
+        object18.setNama("");
+        object18.setNim("");
+        object18.setAlamat("");
+        arraylist.add(object18);
+
+        Mahasiswa object19 = new Mahasiswa();
+        object19.setNama("");
+        object19.setNim("");
+        object19.setAlamat("");
+        arraylist.add(object19);
+
+        Mahasiswa object20 = new Mahasiswa();
+        object20.setNama("");
+        object20.setNim("");
+        object20.setAlamat("");
+        arraylist.add(object20);
+
+        Mahasiswa object21 = new Mahasiswa();
+        object21.setNama("Dzaky");
+        object21.setNim("D0224008");
+        object21.setAlamat("Pamboang");
+        arraylist.add(object21);
+
+        for (Mahasiswa mahasiswa : arraylist) {
+            System.out.println(mahasiswa);
+        }
+
+    }
+}
