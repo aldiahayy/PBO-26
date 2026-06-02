49291f187758b534c35aef23af00e02776624521
aldi <aldisalam0756@gmail.com>
2026-06-03 00:24:17 +0800

    class AnimasiRunnable

---
+++ INHERITANCE/tugas praktikum/soal07/animasisederhana/AnimasiRunnable.java
@@ -0,0 +1,17 @@
+package praktikum5.animasisederhana;
+public class AnimasiRunnable implements Runnable {
+    @Override
+    public void run() {
+        try {
+            for (int i = 1; i >= 5; i++) {
+                System.out.println("Animasi Runnable ke-" + i);
+
+                // Delay 1 detik
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+
+            System.out.println("Runnable terganggu");
+        }
+    }
+}
