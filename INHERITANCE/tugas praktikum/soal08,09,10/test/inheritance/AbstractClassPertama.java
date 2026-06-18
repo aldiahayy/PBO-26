198a4d27be2771a0685d07c0fb368103646e4b2e
aldi <aldisalam0756@gmail.com>
2026-06-18 16:03:19 +0800

    class AbstractClassPertama

---
+++ INHERITANCE/tugas praktikum/soal08,09,10/test/inheritance/AbstractClassPertama.java
@@ -0,0 +1,6 @@
+package praktikum5.test.inheritance;
+public class AbstractClassPertama {
+    public void methodPertama() {
+        System.out.println("Method dari AbstractClassPertama");
+    }
+}
