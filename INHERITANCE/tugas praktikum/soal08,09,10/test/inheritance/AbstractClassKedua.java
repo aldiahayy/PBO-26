f1c105efb54bd320581f8d5981b04fc576bea335
aldi <aldisalam0756@gmail.com>
2026-06-18 16:04:32 +0800

    class AbstractClassKedua

---
+++ INHERITANCE/tugas praktikum/soal08,09,10/test/inheritance/AbstractClassKedua.java
@@ -0,0 +1,6 @@
+package praktikum5.test.inheritance;
+public class AbstractClassKedua extends AbstractClassPertama {
+    public void methodKedua() {
+        System.out.println("Method dari AbstractClassKedua");
+    }
+}
