0151f379310b20bee4d5e25966612c471e24d5d2
aldi <aldisalam0756@gmail.com>
2026-06-23 01:24:34 +0800

    class Segitiga

---
+++ INHERITANCE/tugas praktikum/soal12/Segitiga.java
@@ -0,0 +1,19 @@
+package praktikum5;
+public class Segitiga extends BangunDatar {
+    private double alas;
+    private double tinggi;
+
+    // Constructor
+    public Segitiga(double alas, double tinggi) {
+        super("Segitiga");
+
+        this.alas = alas;
+        this.tinggi = tinggi;
+    }
+
+    // Overriding
+    @Override
+    public double hitungLuas() {
+        return 0.5 * alas * tinggi;
+    }
+}
