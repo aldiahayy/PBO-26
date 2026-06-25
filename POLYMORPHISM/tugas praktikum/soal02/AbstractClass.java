3747faf97c42b77c26e804c4eb4c8275a13a6297
aldi <aldisalam0756@gmail.com>
2026-06-25 13:59:25 +0800

    class AbstractClass

---
+++ POLYMORPHISM/tugas praktikum/soal02/AbstractClass.java
@@ -0,0 +1,25 @@
+package praktikum6.soal02;
+public abstract class AbstractClass {
+    // Method Abstract
+    public abstract void abstractMethod01();
+    public abstract void abstractMethod02();
+    public abstract void abstractMethod03();
+    public abstract void abstractMethod04();
+    public abstract void abstractMethod05();
+    public abstract void abstractMethod06();
+    public abstract void abstractMethod07();
+    public abstract void abstractMethod08();
+    public abstract void abstractMethod09();
+    public abstract void abstractMethod10();
+
+    public void nonAbstractMethod01(){}
+    public void nonAbstractMethod02(){}
+    public void nonAbstractMethod03(){}
+    public void nonAbstractMethod04(){}
+    public void nonAbstractMethod05(){}
+    public void nonAbstractMethod06(){}
+    public void nonAbstractMethod07(){}
+    public void nonAbstractMethod08(){}
+    public void nonAbstractMethod09(){}
+    public void nonAbstractMethod10(){}
+}
