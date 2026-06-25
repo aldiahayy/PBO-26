1de781a06ba66783e45c7b9ebcdf7b7b0cda9f74
aldi <aldisalam0756@gmail.com>
2026-06-26 04:59:44 +0800

    interface Interface2

---
+++ POLYMORPHISM/tugas praktikum/soal02/Interface2.java
@@ -0,0 +1,13 @@
+package praktikum6.soal02;
+public interface Interface2 {
+    void interfaceMethod11();
+    void interfaceMethod12();
+    void interfaceMethod13();
+    void interfaceMethod14();
+    void interfaceMethod15();
+    void interfaceMethod16();
+    void interfaceMethod17();
+    void interfaceMethod18();
+    void interfaceMethod19();
+    void interfaceMethod20();
+}
