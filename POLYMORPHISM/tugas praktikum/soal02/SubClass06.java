856b0f1f9bf2d15d38e2e26c4089e4605c765260
aldi <aldisalam0756@gmail.com>
2026-06-28 20:28:45 +0800

    class SubClass06

---
+++ POLYMORPHISM/tugas praktikum/soal02/SubClass06.java
@@ -0,0 +1,203 @@
+package praktikum6.soal02;
+public class SubClass06 extends AbstractClass implements Interface1, Interface2{
+    @Override
+    public void abstractMethod01() {
+
+    }
+
+    @Override
+    public void abstractMethod02() {
+
+    }
+
+    @Override
+    public void abstractMethod03() {
+
+    }
+
+    @Override
+    public void abstractMethod04() {
+
+    }
+
+    @Override
+    public void abstractMethod05() {
+
+    }
+
+    @Override
+    public void abstractMethod06() {
+
+    }
+
+    @Override
+    public void abstractMethod07() {
+
+    }
+
+    @Override
+    public void abstractMethod08() {
+
+    }
+
+    @Override
+    public void abstractMethod09() {
+
+    }
+
+    @Override
+    public void abstractMethod10() {
+
+    }
+
+    @Override
+    public void nonAbstractMethod10() {
+        super.nonAbstractMethod10();
+    }
+
+    @Override
+    public void nonAbstractMethod09() {
+        super.nonAbstractMethod09();
+    }
+
+    @Override
+    public void nonAbstractMethod08() {
+        super.nonAbstractMethod08();
+    }
+
+    @Override
+    public void nonAbstractMethod07() {
+        super.nonAbstractMethod07();
+    }
+
+    @Override
+    public void nonAbstractMethod06() {
+        super.nonAbstractMethod06();
+    }
+
+    @Override
+    public void nonAbstractMethod05() {
+        super.nonAbstractMethod05();
+    }
+
+    @Override
+    public void nonAbstractMethod04() {
+        super.nonAbstractMethod04();
+    }
+
+    @Override
+    public void nonAbstractMethod03() {
+        super.nonAbstractMethod03();
+    }
+
+    @Override
+    public void nonAbstractMethod02() {
+        super.nonAbstractMethod02();
+    }
+
+    @Override
+    public void nonAbstractMethod01() {
+        super.nonAbstractMethod01();
+    }
+
+
+    @Override
+    public void interfaceMethod01() {
+
+    }
+
+    @Override
+    public void interfaceMethod02() {
+
+    }
+
+    @Override
+    public void interfaceMethod03() {
+
+    }
+
+    @Override
+    public void interfaceMethod04() {
+
+    }
+
+    @Override
+    public void interfaceMethod05() {
+
+    }
+
+    @Override
+    public void interfaceMethod06() {
+
+    }
+
+    @Override
+    public void interfaceMethod07() {
+
+    }
+
+    @Override
+    public void interfaceMethod08() {
+
+    }
+
+    @Override
+    public void interfaceMethod09() {
+
+    }
+
+    @Override
+    public void interfaceMethod10() {
+
+    }
+
+    @Override
+    public void interfaceMethod11() {
+
+    }
+
+    @Override
+    public void interfaceMethod12() {
+
+    }
+
+    @Override
+    public void interfaceMethod13() {
+
+    }
+
+    @Override
+    public void interfaceMethod14() {
+
+    }
+
+    @Override
+    public void interfaceMethod15() {
+
+    }
+
+    @Override
+    public void interfaceMethod16() {
+
+    }
+
+    @Override
+    public void interfaceMethod17() {
+
+    }
+
+    @Override
+    public void interfaceMethod18() {
+
+    }
+
+    @Override
+    public void interfaceMethod19() {
+
+    }
+
+    @Override
+    public void interfaceMethod20() {
+
+    }
+}
