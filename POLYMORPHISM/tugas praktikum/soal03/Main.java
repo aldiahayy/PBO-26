e91165ec597b539d9b470115967a5c34ec4ae0c1
aldi <aldisalam0756@gmail.com>
2026-07-04 01:36:34 +0800

    soal03
    3. Buatlah sepuluh class (ClassA, ClassB, ClassC, ClassD, ClassE, ClassF, ClassG, ClassH, ClassI, ClassJ). Disetiap class terdapat overriding ke method public String toString() { }. Method toString ini akan mengembalikan (return) String nama class tempat method toString( ) tersebut ditulis. Dengan class yang extends masing-masing sebagai berikut:
    ClassB extends ClassA
    ClassC extends ClassB
    ClassD extends ClassC
    ClassE extends ClassD
    ClassF extends ClassE
    ClassG extends ClassF
    ClassH extends ClassG
    ClassI extends ClassH
    ClassJ extends ClassI
    Buatlah sebuah class lagi yang memiliki method main. di dalam method main buatlah object dari spuluh class tersebut (ClassA, ClassB, ClassC, ClassD, ClassE, ClassF, ClassG, ClassH, ClassI, ClassJ) lalu lakukan pemanggilan method toString dari sepuluh object yang telah dibuat.

---
+++ POLYMORPHISM/tugas praktikum/soal03/Main.java
@@ -0,0 +1,26 @@
+package praktikum6.soal03;
+public class Main {
+    public static void main(String[] args) {
+        ClassA ca = new ClassA();
+        ClassB cb = new ClassB();
+        ClassC cc = new ClassC();
+        ClassD cd = new ClassD();
+        ClassE ce = new ClassE();
+        ClassF cf = new ClassF();
+        ClassG cg = new ClassG();
+        ClassH ch = new ClassH();
+        ClassI ci = new ClassI();
+        ClassJ cj = new ClassJ();
+
+        System.out.println(ca);
+        System.out.println(cb);
+        System.out.println(cc);
+        System.out.println(cd);
+        System.out.println(ce);
+        System.out.println(cf);
+        System.out.println(cg);
+        System.out.println(ch);
+        System.out.println(ci);
+        System.out.println(cj);
+    }
+}
